db35b53d9fa83308a833e682806b79ef81da9dc9
Javid Dadashkarimi of group gerstein <jd2392@farnam1.farnam.hpc.yale.internal>
2018-08-08 08:56:00 -0400

    new changes

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 #SBATCH --partition=general
 #SBATCH --job-name=javid
 #SBATCH --ntasks=1 --nodes=4
-#SBATCH --mem-per-cpu=25000 
-#SBATCH --mem-=250000
+#SBATCH --mem-per-cpu=55000 
+#SBATCH --mem-=550000
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=email
-python train.py -file rest1_LR.mat.csv  unlabled.csv -m svr -semi sr
+python train.py -file rest1_LR.mat.csv  unlabled.csv -m svr -semi svr
 #python train.py -file rest1_LR.mat.csv -m svr
