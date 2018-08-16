25a0ba6a3253ed5a71efb9a1dba419cdc2e8443e
Javid Dadashkarimi of group gerstein <jd2392@farnam1.farnam.hpc.yale.internal>
2018-08-16 10:15:09 -0400

    gaussian kernel

--- imaging/train.py
+++ imaging/train.py
@@ -43,29 +43,25 @@ def rot(image, xy, angle):
 def transformation(filename):
 	from nilearn import plotting 
 	from nilearn import datasets
-	x0,y0 = 300,200
+	import scipy.ndimage as ndimage
 	img = nib.load(filename)
 	print(img.get_fdata().shape)
   	flip_x = np.flip(img.get_fdata(),0)
   	flip_y = np.flip(img.get_fdata(),1)
   	flip_z = np.flip(img.get_fdata(),2)
-	#flip_x.to_filename('more_smooth_anat_img.nii.gz')		
-	new_img = nib.Nifti1Image(flip_x,affine = np.eye(4))
-	nib.save(new_img, os.path.join('images','flip_x.'+filename.split('/')[-1]))
-	#data_orig = img.get_fdata()[...,1]#misc.face()
-	#print(data_orig.shape)
-	#fig,axes = plt.subplots(1,2)
-	#axes[0].imshow(data_orig,cmap = plt.cm.gray, vmin = 10, vmax = 200)
-	#axes[0].scatter(x0,y0,c="r" )
-	#axes[0].set_title("original")
-	#data_rot, (x1,y1) = rot(data_orig, np.array([x0,y0]), 66)
-	##data_rot = rot(data_orig, 66)
-	#axes[1].imshow(data_rot,cmap = plt.cm.gray)
-	#axes[1].scatter(x1,y1,c="r" )
-	#axes[1].set_title("Rotation: {} deg".format(66))
-	#plt.show()
-	##plotting.plot_roi(img, cmap=plotting.cm.bwr_r,title='yale imaging')
-	#plotting.show()
+	flip_img_x = nib.Nifti1Image(flip_x,affine = np.eye(4))
+	flip_img_y = nib.Nifti1Image(flip_y,affine = np.eye(4))
+	flip_img_z = nib.Nifti1Image(flip_z,affine = np.eye(4))
+	gaussian_img = nib.Nifti1Image(ndimage.gaussian_filter(img.get_fdata(),sigma=(5,5,0), order=0),affine=np.eye(4))
+	from nilearn import image
+	smooth_img = image.smooth_img(img,[50.1,10.0,0.2])
+	nib.save(flip_img_x, os.path.join('images','flip_x.'+filename.split('/')[-1]))
+	nib.save(flip_img_y, os.path.join('images','flip_y.'+filename.split('/')[-1]))
+	nib.save(flip_img_z, os.path.join('images','flip_z.'+filename.split('/')[-1]))
+	nib.save(smooth_img, os.path.join('images','smooth.'+filename.split('/')[-1]))
+	print(type(smooth_img))
+	print(type(gaussian_img))
+	nib.save(gaussian_img, os.path.join('images','gaussian.'+filename.split('/')[-1]))
 
 def parcelation(filename):
 	from nilearn import plotting 
@@ -76,7 +72,7 @@ def parcelation(filename):
 	networks_444 = parcellations['scale444']
 	x0,y0 = 580,300
 	img = nib.load(filename)
-	data_orig = img.get_fdata()[1,...]#misc.face()
+	data_orig = img.get_fdata()[...,1]#misc.face()
 	print(data_orig.shape)
 	fig,axes = plt.subplots(1,2)
 	axes[0].imshow(data_orig)
